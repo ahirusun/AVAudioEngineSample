67ca00c74b1fe3924983739dc1e3ce71e1f4b61f
Kouhei Natori <kouhei-natori@united.jp>
2016-06-10 10:15:06 +0900

    Initial Commit

---
+++ AudioEngineSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioEngineSample
+//
+//  Created by 名取弘平 on 2016/06/10.
+//  Copyright © 2016年 ahirusun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
